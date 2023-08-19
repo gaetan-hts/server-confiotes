beca1db617a6fe6980859ded128d1bf71d90d199
GRE3DISGOOD <gaetanhonthaas@gmail.com>
2023-08-19 18:17:46 +0200

    push to ionos

--- config/plugins.js
+++ config/plugins.js
@@ -1,28 +1,28 @@
-module.exports = ({ env }) => ({
-  // email: {
-  //   config: {
-  //     provider: "sendgrid",
-  //     providerOptions: {
-  //       apiKey: env("SENDGRID_API_KEY"),
-  //     },
-  //     settings: {
-  //       defaultFrom: "gaetanhonthaas@gmail.com",
-  //       defaultReplyTo: "gaetanhonthaas@gmail.com",
-  //     },
-  //   },
-  // },
-  upload: {
-    config: {
-      provider: "cloudinary",
-      providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
-      },
-      actionOptions: {
-        upload: {},
-        delete: {},
-      },
-    },
-  },
-});
+// module.exports = ({ env }) => ({
+//   // email: {
+//   //   config: {
+//   //     provider: "sendgrid",
+//   //     providerOptions: {
+//   //       apiKey: env("SENDGRID_API_KEY"),
+//   //     },
+//   //     settings: {
+//   //       defaultFrom: "gaetanhonthaas@gmail.com",
+//   //       defaultReplyTo: "gaetanhonthaas@gmail.com",
+//   //     },
+//   //   },
+//   // },
+//   upload: {
+//     config: {
+//       provider: "cloudinary",
+//       providerOptions: {
+//         cloud_name: env("CLOUDINARY_NAME"),
+//         api_key: env("CLOUDINARY_KEY"),
+//         api_secret: env("CLOUDINARY_SECRET"),
+//       },
+//       actionOptions: {
+//         upload: {},
+//         delete: {},
+//       },
+//     },
+//   },
+// });
